9e90d55bc52c7729edad5cfcfb034dc1df2fb6c7
Jake Gillman <jakegillman3@gmail.com>
2023-11-04 21:00:30 +0000

    sockets

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import subprocess
 import platform
 import os
 import time as t
+import socket
+import json
 
 def tail(fn):
 	fp = open(fn, 'r')
@@ -13,30 +15,37 @@ def tail(fn):
 			t.sleep(0.5)
 
 def main():
-	files = os.listdir()
-	if platform.system() == 'Windows':
-		subprocess.Popen(["qrt_data_extraction-Windows_x64.exe"])
-	elif platform.system() == 'Darwin':
-		subprocess.Popen(["./qrt_data_extraction.MacOS"])
-	elif platform.system() == 'Linux':
-		subprocess.Popen(["./qrt_data_extraction.Linux_x86_x64"])
-	t.sleep(1)
-	newfiles = os.listdir()
-	for file in newfiles:
-		if file not in files:
-			filename = file
-			break
-	print(filename)
-	
-	for line in tail(filename):
-		try:
-			date = line.split(" ")[0]
-			time = line.split(" ")[1]
-			type = line.split(" ")[2][:-1]
-			message = ' '.join(line.split(" ")[3:])
-		except IndexError:
-			continue
-		print(date, time, type, message)
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect(("127.0.0.1", 65432))
+		files = os.listdir()
+		if platform.system() == 'Windows':
+			subprocess.Popen(["qrt_data_extraction-Windows_x64.exe"])
+		elif platform.system() == 'Darwin':
+			subprocess.Popen(["./qrt_data_extraction.MacOS"])
+		elif platform.system() == 'Linux':
+			subprocess.Popen(["./qrt_data_extraction.Linux_x86_x64"])
+		t.sleep(1)
+		newfiles = os.listdir()
+		for file in newfiles:
+			if file not in files:
+				filename = file
+				break
+		for line in tail(filename):
+			try:
+				date = line.split(" ")[0]
+				time = line.split(" ")[1]
+				type = line.split(" ")[2][:-1]
+				message = ' '.join(line.split(" ")[3:])
+			except IndexError:
+				continue
+			jsona = {
+				"date": date,
+				"time": time,
+				"type": type,
+				"message": message
+			}
+			s.sendall(json.dumps(jsona).encode('utf-8'))
+			print(date, time, type, message)
 
 if __name__ == "__main__":
 	main()
