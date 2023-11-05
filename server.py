261f0c289a2ea762d4755a92f0c1687d2e653957
Jake Gillman <jakegillman3@gmail.com>
2023-11-05 01:50:44 +0000

    made it into an api and broken it

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ import os
 import time as t
 import socket
 import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__) 
 
 def tail(fn):
 	fp = open(fn, 'r')
@@ -14,39 +17,42 @@ def tail(fn):
 		else:
 			t.sleep(0.5)
 
-def main():
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect(("127.0.0.1", 65432))
-		files = os.listdir()
-		if platform.system() == 'Windows':
-			subprocess.Popen(["qrt_data_extraction-Windows_x64.exe"])
-		elif platform.system() == 'Darwin':
-			subprocess.Popen(["./qrt_data_extraction.MacOS"])
-		elif platform.system() == 'Linux':
-			subprocess.Popen(["./qrt_data_extraction.Linux_x86_x64"])
-		t.sleep(1)
-		newfiles = os.listdir()
-		for file in newfiles:
-			if file not in files:
-				filename = file
-				break
-		for line in tail(filename):
-			try:
-				date = line.split(" ")[0]
-				time = line.split(" ")[1]
-				type = line.split(" ")[2][:-1]
-				message = ' '.join(line.split(" ")[3:])
-			except IndexError:
-				continue
-			jsona = {
-				"date": date,
-				"time": time,
-				"type": type,
-				"message": message
-			}
-			s.sendall(json.dumps(jsona).encode('utf-8'))
-			t.sleep(0.1)
-			print(date, time, type, message)
+@app.route('/', methods = ['POST']) 
+def home():
+	if request.method == 'POST':
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.connect(("127.0.0.1", 65432))
+			files = os.listdir()
+			if platform.system() == 'Windows':
+				subprocess.Popen(["qrt_data_extraction-Windows_x64.exe"])
+			elif platform.system() == 'Darwin':
+				subprocess.Popen(["./qrt_data_extraction.MacOS"])
+			elif platform.system() == 'Linux':
+				subprocess.Popen(["./qrt_data_extraction.Linux_x86_x64"])
+			t.sleep(1)
+			newfiles = os.listdir()
+			for file in newfiles:
+				if file not in files:
+					filename = file
+					break
+			for line in tail(filename):
+				try:
+					date = line.split(" ")[0]
+					time = line.split(" ")[1]
+					type = line.split(" ")[2][:-1]
+					message = ' '.join(line.split(" ")[3:])
+				except IndexError:
+					continue
+				jsona = {
+					"date": date,
+					"time": time,
+					"type": type,
+					"message": message
+				}
+				s.sendall(json.dumps(jsona).encode('utf-8'))
+				t.sleep(0.25)
+				print(date, time, type, message)
+		return jsonify({"status": "success"})
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':   
+    app.run(port=8000, debug=True)
